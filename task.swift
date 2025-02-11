ef79a7687bc882e933491619e6802b24211dc597
Steven <116920160+StevenLuque2003@users.noreply.github.com>
2025-02-10 20:13:10 -0500

    Update task.swift

--- task.swift
+++ task.swift
@@ -1,1 +1,10 @@
+import UIKit
+import CoreLocation
 
+struct Task {
+    var title: String
+    var description: String
+    var image: UIImage?
+    var location: CLLocationCoordinate2D?
+    var isCompleted: Bool = false
+}
