b853f50dde9f0924587462bc972a5f0fe22397e4
Steven <116920160+StevenLuque2003@users.noreply.github.com>
2025-02-10 20:13:26 -0500

    Update TaskListViewController.swift

--- TaskListViewController.swift
+++ TaskListViewController.swift
@@ -1,1 +1,34 @@
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    var tasks: [Task] = [
+        Task(title: "Your favorite hiking spot", description: "Where do you go to be one with nature?"),
+        Task(title: "Best coffee shop", description: "Find the best coffee in town!"),
+        Task(title: "A cool street mural", description: "Find and capture street art."),
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Scavenger Hunt"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        cell.accessoryType = task.isCompleted ? .checkmark : .none
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let taskDetailVC = TaskDetailViewController(task: tasks[indexPath.row])
+        navigationController?.pushViewController(taskDetailVC, animated: true)
+    }
+}
 
